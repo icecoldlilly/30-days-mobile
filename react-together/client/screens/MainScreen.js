670178264353996d770f0f8bdfbf7b72e5988d1d
Hilal Habashi <3894104+hilalh@users.noreply.github.com>
2018-01-30 11:08:44 +0200

    Added buttonStyle to buttons in MainScreen and added icons as well

--- react-together/client/screens/MainScreen.js
+++ react-together/client/screens/MainScreen.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 import { Button, Icon } from 'react-native-elements';
-
+import "@expo/vector-icons"; // 6.2.2
 export default class MainScreen extends Component {
+  static ChatIcon = <Icon name="fa-commenting-o" type="font-awesome" />
   static navigationOptions = {
     title: 'Together',
     // This removes back button title
@@ -13,9 +14,19 @@ export default class MainScreen extends Component {
     return (
       <View style={styles.container}>
         <Button 
-        text='Messenger 💬'
+        large
+        text={'Messenger'}
+        icon={ <Icon name="commenting-o" color="white" type="font-awesome" /> }
+        buttonStyle={{backgroundColor: '#0083ff', borderRadius: 10}}
+        textStyle={{textAlign: 'center'}}
         onPress={() => navigate('Messneger')} />
-        <Button text='YouTube  📺' onPress={() => navigate('YouTube')} />
+        <Button
+        large
+        text='YouTube'
+        icon={ <Icon name="youtube-play" color="white" type="font-awesome" /> }
+        buttonStyle={{backgroundColor: 'red', borderRadius: 10}}
+        textStyle={{textAlign: 'center'}}
+        onPress={() => navigate('YouTube')} />
       </View>
     );
   }

ef57719ffea8bd001ec42e880bc273c7d6224ccb
Hilal Habashi <3894104+hilalh@users.noreply.github.com>
2018-01-30 16:50:28 +0200

    Added environment variables using 'react-native-dotnev'

--- react-together/client/components/YouTube/PlayerUI.js
+++ react-together/client/components/YouTube/PlayerUI.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { GOOGLE_API_KEY } from 'react-native-dotenv';
 /* 
   TODO: ✍🏽 Add video player here
    */

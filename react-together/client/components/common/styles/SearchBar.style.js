6aa462bd821a8bdde4b926c8137c429e437e35b2
Hilal Habashi <3894104+hilalh@users.noreply.github.com>
2018-01-31 12:57:26 +0200

    Added SearchBar.style.js to help style our Search Bars

---
+++ react-together/client/components/common/styles/SearchBar.style.js
@@ -0,0 +1,27 @@
+mport {
+  StyleSheet,
+  Platform
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center', // align horizontal
+    justifyContent: 'center', // align vertical
+    flexDirection: 'row',
+  },
+  inputSearch: {
+    padding: 5,
+    borderColor: 'gray',
+    borderWidth: (Platform.OS === 'ios') ? 1 : 0,
+    borderRadius: 5,
+    height: 40,
+    flex: 1,
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    alignSelf: 'center',
+  }
+});
+
+module.exports = styles;

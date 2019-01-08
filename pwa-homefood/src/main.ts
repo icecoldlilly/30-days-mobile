2fcd9b8ab046f28e6cc6bb3065f422cdbc404f9c
Hilal Habashi <3894104+hilalh@users.noreply.github.com>
2019-01-08 13:49:35 +0200

    AWS works!; TODO: Fix styling for AWS components

--- pwa-homefood/src/main.ts
+++ pwa-homefood/src/main.ts
@@ -7,8 +7,10 @@ import './registerServiceWorker';
 
 import DefaultLayout from '@/layouts/DefaultLayout.vue'; // @ is an alias to /src
 import AppLayout from '@/layouts/AppLayout.vue'; // @ is an alias to /src
+import AuthLayout from '@/layouts/AuthLayout.vue'; // @ is an alias to /src
 Vue.component('default-layout', DefaultLayout);
 Vue.component('app-layout', AppLayout);
+Vue.component('auth-layout', AuthLayout);
 
 Vue.config.productionTip = false;
 
--- pwa-homefood/src/plugins/amplify.ts
+++ pwa-homefood/src/plugins/amplify.ts
@@ -1,7 +1,4 @@
-// // import Vue from 'vue';
-// import Amplify, * as AmplifyModules from 'aws-amplify';
-// import AmplifyPlugin from 'aws-amplify-vue';
-// // import aws_exports from '@/aws-exports.js';
-// Amplify.configure({});
-
-// Vue.use(AmplifyPlugin, AmplifyModules);
+import Vue from 'vue';
+import Amplify from 'aws-amplify';
+import aws_exports from '@/aws-exports.js';
+Amplify.configure(aws_exports);
